c62dc91033f3b244f4ed9b6fceba44d153b35e00
Faizal Adhitama <faizaladhitamaprabowo@gmail.com>
2017-04-24 12:20:53 +0700

    push

---
+++ Source_Code/fungsi_a/SpecifiedMichalewiczFunction.m
@@ -0,0 +1,18 @@
+function [y,z] = SpecifiedMichalewiczFunction(x,t)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+if (nargin < 2), t = 1; end
+n = power(8,t);
+temp = 0;
+for i=1:n
+    temp_x = x;
+    if mod(n,2) == 1
+        temp_x = x(0);
+    else
+        temp_x = x(1);
+    end
+    temp = temp + sin(temp_x)*power(sin(i*power(temp_x,2)/pi),20);
+end
+y = -temp
+z = gradient(y);
+end
